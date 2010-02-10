9027fdfe532af69aef0e5e30ffb1e9f20c1e587e
Egon Willighagen <egon.willighagen@gmail.com>
2010-02-10 21:10:36 +0100

    Updated for a API change: method rename for getting a JSConsole manager

--- plugins/net.bioclipse.moss/src/net/bioclipse/moss/MossRunner.java
+++ plugins/net.bioclipse.moss/src/net/bioclipse/moss/MossRunner.java
@@ -154,7 +154,7 @@ public class MossRunner {
         miner.run();
         miner.stats();
 
-        net.bioclipse.scripting.ui.Activator.getDefault().getJsConsoleManager().say(bo.toString());
+        net.bioclipse.scripting.ui.Activator.getDefault().getJavaJsConsoleManager().say(bo.toString());
 
         try {
             bo.close();
